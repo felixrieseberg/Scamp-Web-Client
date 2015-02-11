7e808c2f0975bbb0f55b4f1be878db856aa9e552
Felix Rieseberg <felix@felixrieseberg.com>
2015-02-10 16:25:56 -0800

    Dashboard Update

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    breadCrumb: 'Dashboard'
+    breadCrumb: 'Dashboard',
+
+    info: function() {
+        console.log(this.get('infos'));
+        return this.get('infos');
+    }.property('infos')
 });
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
     model: function () {
-        return this.store.find('resource');
-    }
+        return Ember.Object.create({
+            resources: this.store.find('resource'),
+            groups: this.store.find('group')
+        });
+    },
 
+    setupController: function (controller, model) {
+        this._super(controller, model);
+
+        controller.set('resources', model.get('resources'));
+        controller.set('groups', model.get('groups'));
+
+        // Set various general info things
+        controller.set('averageUtilization', 87);
+    }
 });
